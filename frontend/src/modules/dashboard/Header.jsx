39d8e12f60777e860baa9199270da77183c24c1f
susant-dev <susantshrestha268@gmail.com>
2025-11-16 09:41:59 -0500

    added: navigation in header title

--- frontend/src/modules/dashboard/Header.jsx
+++ frontend/src/modules/dashboard/Header.jsx
@@ -1,12 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../shared/Button";
 import { useCartContext } from "../shared/CartContext";
 
 function Header() {
+  const navigate = useNavigate();
   const { products } = useCartContext();
 
   return (
     <header>
-      <h1 className="title">Dummy Online Shop</h1>
+      <div className="title">
+        <h1
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/");
+          }}
+        >
+          Dummy Online Shop
+        </h1>
+      </div>
       <Button onClick={() => {}}>Cart ({products.length})</Button>
     </header>
   );
